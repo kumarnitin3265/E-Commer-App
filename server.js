518f3e380fa0a0e8888e13382931d84933579cd0
Nitin Kumar <kumar.nitin0812@gmail.com>
2024-07-14 00:11:07 +0530

    Complete Project

---
+++ server.js
@@ -0,0 +1,1 @@
+// Complete the server.js file to make user's add, delete and update the todos.
---
+++ task.schema.js
@@ -0,0 +1,12 @@
+// no need to change the prewritten code
+// make necessary imports here
+
+// Define the Task schema
+const taskSchema = new mongoose.Schema({
+    // ------write your code here.-------
+});
+
+// Create a Task model from the schema
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
